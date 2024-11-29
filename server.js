37d1abbfabf9d05f478e2c30b7524a039ea7a59c
md1509 <mohammedhatem1509@gmail.com>
2024-11-29 10:56:07 +0300

    Update specific file: server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Schema and model
 const submissionSchema = new mongoose.Schema({
